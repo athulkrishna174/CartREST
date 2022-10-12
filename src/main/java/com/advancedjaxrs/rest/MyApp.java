66d49e056acb5bec28e59e1a2af3232cac24607c
Athul Krishna P S <athul.krishna@m.ecsfin.com>
2022-10-12 11:43:38 +0530

    Initial commit

---
+++ src/main/java/com/advancedjaxrs/rest/MyApp.java
@@ -0,0 +1,10 @@
+package com.advancedjaxrs.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("webapi")
+public class MyApp extends Application{
+
+}
