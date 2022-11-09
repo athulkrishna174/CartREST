076d64e52ef65ddbc1c95d3cdac6525c0df2ef9e
Athul Krishna P S <athul.krishna@m.ecsfin.com>
2022-11-09 16:34:16 +0530

    Added cart services

--- src/main/java/com/cartrest/resources/CartResource.java
+++ src/main/java/com/cartrest/resources/CartResource.java
@@ -1,12 +1,16 @@
 package com.cartrest.resources;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.cartrest.model.Item;
 import com.cartrest.services.CartService;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -14,20 +18,38 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/cartitems")
 @Produces(value = {MediaType.APPLICATION_JSON})
+@Consumes(value = {MediaType.APPLICATION_JSON})
 public class CartResource {
     
     private CartService cartService = new CartService();
     
     @GET
     @Path("/{userId}")
-    public List<Item> getItems(@PathParam("userId") int userId) throws Exception {
+    public List<Item> getItems(@PathParam("userId") int userId) throws SQLException {
         return cartService.getCartItems(userId);
     }
     
     @DELETE
-    @Path("/{userId}/{productId}")
-    public boolean deleteItem(@PathParam("userId") int userId, @PathParam("productId") int productId) throws Exception{
-        return cartService.deleteItem(userId, productId);
+    @Path("/{userId}/{id}")
+    public boolean deleteItem(@PathParam("userId") int userId, @PathParam("id") int id){
+        return cartService.deleteItem(userId, id);
     }
-
+    
+    @DELETE
+    @Path("/{userId}")
+    public boolean deleteAllItems(@PathParam("userId") int userId) {
+        return cartService.deleteAllItems(userId);
+    }
+    
+    @POST
+    @Path("/{userId}")
+    public boolean insertItem(@PathParam("userId") int userId, Item item)throws SQLException{
+        return cartService.inserItem(userId, item);
+    }
+    
+    @PUT
+    @Path("/{userId}")
+    public boolean updateOrders(@PathParam("userId") int userId, List<Item> items)throws SQLException{
+        return cartService.updateMyOrder(userId, items);
+    }
 }
--- src/main/java/com/cartrest/services/CartService.java
+++ src/main/java/com/cartrest/services/CartService.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.cartrest.database.DatabaseConnection;
@@ -13,12 +15,12 @@ import com.cartrest.model.Item;
 public class CartService {
     private DatabaseConnection connector = new DatabaseConnection();
     
-    public List<Item> getCartItems(int UserId) throws Exception{
+    public List<Item> getCartItems(int userId) throws SQLException{
         Connection connection = connector.getConnection();
         
         String query = "SELECT * FROM cartitems WHERE uid = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, UserId);
+            statement.setInt(1, userId);
             
             ResultSet rs = statement.executeQuery();
       
@@ -46,13 +48,13 @@ public class CartService {
             return items;
         }
     
-    public boolean deleteItem(int userId, int productId){
+    public boolean deleteItem(int userId, int id){
         Connection connection = connector.getConnection();
         
-        String query = "DELETE FROM cartitems WHERE uid = ? and pid = ?";
+        String query = "DELETE FROM cartitems WHERE uid = ? and id = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, userId);
-            statement.setInt(2, productId);
+            statement.setInt(2, id);
             
             statement.execute();
             return true;
@@ -61,5 +63,64 @@ public class CartService {
         }
     }
     
+    public boolean deleteAllItems(int userId) {
+        Connection connection = connector.getConnection();
+        
+        String query = "DELETE FROM cartitems WHERE uid = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, userId);
+            
+            statement.execute();
+            return true;
+        }catch (Exception e) {
+            return false;
+        }
+    }
+    
+    
+    public boolean inserItem(int userId, Item item) throws SQLException {
+        Connection connection = connector.getConnection();
+        
+        String query = "INSERT INTO cartitems (uid, pid, name, quantity, price, total)"
+                + "VALUES (?,?,?,?,?,?)";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, userId);
+            statement.setInt(2, item.getPid());
+            statement.setString(3, item.getName());
+            statement.setInt(4, item.getQuantity());
+            statement.setInt(5, item.getPrice());
+            statement.setInt(6, item.getTotal());
+            
+            statement.execute();
+      
+        }
+        return true;
+    }
+    
+    public boolean updateMyOrder(int userId, List<Item> items) throws SQLException {
+        
+        Connection connection = connector.getConnection();
+        
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");  
+        Date date = new Date();
+        
+        for(Item item: items) {
+            String query = "INSERT INTO orders (uid, pid, name,  price, quantity, total, date)"
+                    + "VALUES (?,?,?,?,?,?,?)";
+            
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setInt(1, userId);
+                statement.setInt(2, item.getId());
+                statement.setString(3, item.getName());
+                statement.setInt(4, item.getPrice());
+                statement.setInt(5, item.getQuantity());
+                statement.setInt(6, item.getTotal());
+                statement.setString(7, formatter.format(date));
+                
+                statement.execute();
+            }
+        }
+        return true;
+    }
     
 }
